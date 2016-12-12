2b0d8bb0a2d9518aee35ef2b9f05ba90af6f1b3f
RaydonZhu <raydon@stagwell.tech>
2016-12-12 14:20:31 -0500

    d file

--- hub-test.d.ts
+++ hub-test.d.ts
@@ -1,2 +1,7 @@
 export * from './src/app/hub-test.component';
 export * from './src/app/hub.module';
+export * from './src/app/model/hub-user.model';
+export * from './src/app/model/hub-comment.model';
+export * from './src/app/comment/hub-comment.component';
+export * from './src/app/comment/hub-comment.service';
+export * from './src/app/comment/hub-mock.database';
--- hub-test.js
+++ hub-test.js
@@ -1,2 +1,7 @@
 exports.HubTestComponent = require('./src/app/hub-test.component').HubTestComponent;
 exports.HubModule = require('./src/app/hub.module').HubModule;
+exports.HubUser = require('./src/app/model/hub-user.model').HubUser;
+exports.HubComment = require('./src/app/model/hub-comment.model').HubComment;
+
+exports.HubCommentService = require('./src/app/comment/hub-comment.service').HubCommentService;
+exports.HubMockDatabase = require('./src/app/comment/hub-mock.database').HubMockDatabase;
